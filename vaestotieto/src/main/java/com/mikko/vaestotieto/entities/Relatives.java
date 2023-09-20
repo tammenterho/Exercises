cee5984334bad8c7752e05a7ab50ca261ccd2fe0
tammenterho <terho.mikko@gmail.com>
2023-09-20 09:54:52 +0300

    Relatives entity

--- vaestotieto/src/main/java/com/mikko/vaestotieto/entities/Relatives.java
+++ vaestotieto/src/main/java/com/mikko/vaestotieto/entities/Relatives.java
@@ -1,5 +1,38 @@
 package com.mikko.vaestotieto.entities;
 
 public class Relatives {
+	private String iCAContactName;
+	private String iCAContactNumber;
+	private String iCAContactEmail;
+	
+	public Relatives (String iCAContactName, String iCAContactNumber, String iCAContactEmail) {
+		this.iCAContactName = iCAContactName;
+		this.iCAContactNumber = iCAContactNumber;
+		this.iCAContactEmail = iCAContactEmail;
+	}
 
+	public String getiCAContactName() {
+		return iCAContactName;
+	}
+
+	public void setiCAContactName(String iCAContactName) {
+		this.iCAContactName = iCAContactName;
+	}
+
+	public String getiCAContactNumber() {
+		return iCAContactNumber;
+	}
+
+	public void setiCAContactNumber(String iCAContactNumber) {
+		this.iCAContactNumber = iCAContactNumber;
+	}
+
+	public String getiCAContactEmail() {
+		return iCAContactEmail;
+	}
+
+	public void setiCAContactEmail(String iCAContactEmail) {
+		this.iCAContactEmail = iCAContactEmail;
+	}
+	
 }
