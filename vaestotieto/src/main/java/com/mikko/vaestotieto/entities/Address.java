004f6bacf4f6d4d5b367ee225c62e3a0fd9f1fc2
tammenterho <terho.mikko@gmail.com>
2023-09-20 09:45:46 +0300

    address entity

--- vaestotieto/src/main/java/com/mikko/vaestotieto/entities/Address.java
+++ vaestotieto/src/main/java/com/mikko/vaestotieto/entities/Address.java
@@ -1,5 +1,67 @@
 package com.mikko.vaestotieto.entities;
 
 public class Address {
+	private String street;
+	private String numberStair;
+	private String city;
+	private String postal;
+	
+	
+	
+	public Address(String street, String numberStair, String city, String postal) {
+		this.street = street;
+		this.numberStair = numberStair;
+		this.city = city;
+		this.postal = postal;
+	}
 
+
+
+	public String getStreet() {
+		return street;
+	}
+
+
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+
+
+	public String getNumberStair() {
+		return numberStair;
+	}
+
+
+
+	public void setNumberStair(String numberStair) {
+		this.numberStair = numberStair;
+	}
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+	public String getPostal() {
+		return postal;
+	}
+
+
+
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+
+	
 }
