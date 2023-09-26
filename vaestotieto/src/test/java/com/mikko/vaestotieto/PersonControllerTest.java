7b88c0b4ece00f2e514a9d9c3c6fd460a5ba0031
tammenterho <terho.mikko@gmail.com>
2023-09-26 15:29:21 +0300

    test get by id

--- vaestotieto/src/test/java/com/mikko/vaestotieto/PersonControllerTest.java
+++ vaestotieto/src/test/java/com/mikko/vaestotieto/PersonControllerTest.java
@@ -49,7 +49,24 @@ public class PersonControllerTest {
 		mockMvc.perform(MockMvcRequestBuilders.get("/persons/").contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
-	
+
+	// TESTING GET PERSON BY ID "/persons/{id}"
+	@Test
+	public void testGetByIdPerson() throws Exception {
+		Person person = new Person();
+		person.setId(11L);
+		person.setFirstnames("Liisa Maija");
+		person.setLastNames("Kokkeli");
+
+		when(personService.getPersonById(person.getId())).thenReturn(person);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/persons/{id}", 11L).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(person.getId()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.firstnames").value(person.getFirstnames()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.lastNames").value(person.getLastNames()));
+	}
+
 	// TESTING POST /persons - FILLING FIELDS
 	@Test
 	public void testCreatePerson() throws Exception {
@@ -79,9 +96,9 @@ public class PersonControllerTest {
 		mockMvc.perform(MockMvcRequestBuilders.post("/persons/").content(objectMapper.writeValueAsString(person))
 				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isCreated());
 	}
-	
+
 	// TESTING UPDATE PERSON. 1. CREATING PERSON 2. MODIFYING FIELDS
-	@Test 
+	@Test
 	public void testUpdatePerson() throws Exception {
 		// CREATE A PERSON
 		Person person = new Person();
@@ -143,28 +160,79 @@ public class PersonControllerTest {
 		// https://stackoverflow.com/questions/72241095/spring-mvc-controller-testing-put
 		when(personService.updatePerson(eq(updatedPerson.getId()), any())).thenReturn(updatedPerson); // PERSONSERVICE
 
-		// System.out.println("Before put: " + objectMapper.writeValueAsString(updatedPerson));
+		// System.out.println("Before put: " +
+		// objectMapper.writeValueAsString(updatedPerson));
 
 		mockMvc.perform(MockMvcRequestBuilders.put("/persons/{id}", 11L, updatedPerson)
 				.content(objectMapper.writeValueAsString(updatedPerson)).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 
-		// System.out.println("After put: " + objectMapper.writeValueAsString(updatedPerson));
+		// System.out.println("After put: " +
+		// objectMapper.writeValueAsString(updatedPerson));
 	}
-	
+
 	// TESTING DELETING PERSON "/persons/{id}"
 	@Test
 	public void testDeletePerson() throws Exception {
 		Person person = new Person();
 		person.setId(11L);
-		
+
 		when(personService.deletePerson(11L)).thenReturn(true);
-		
-		mockMvc.perform(MockMvcRequestBuilders.delete("/persons/{id}", 11L).contentType(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.status().isOk());
+
+		mockMvc.perform(MockMvcRequestBuilders.delete("/persons/{id}", 11L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
-	
-	// TESTING UPDATING EMAIL - VALIDATION INCLUDED IN CONTROLLER - SERVICE CLASS TESTING AND CONTROLLER TESTING /persons/{id}/email
+
+	// TESTING GET BY FIRSTNAME - TESTING SERVICE AND CONTROLLER
+	// /persons/firstname/{firstNames}
+	@Test
+	public void testGetByFirstName() throws Exception {
+		Person person = new Person();
+		Person person2 = new Person();
+		person.setFirstnames("Matti");
+		person.setLastNames("Mattilainen");
+		person2.setFirstnames("Liisa");
+		person2.setLastNames("Liisalainen");
+
+		List<Person> persons = new ArrayList<>();
+		persons.add(person);
+		persons.add(person2);
+
+		when(personService.getPersonsByFirstName(person.getFirstnames())).thenReturn(persons);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/persons/firstname/{firstNames}", "Matti")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+
+	}
+
+	// TESTING GET BY LASTNAME - TESTING SERVICE AND CONTROLLER
+	// /persons/lastname/{lastName}
+	@Test
+	public void testGetByLastName() throws Exception {
+		Person person = new Person();
+		Person person2 = new Person();
+		person.setFirstnames("Matti");
+		person.setLastNames("Mattilainen");
+		person2.setFirstnames("Liisa");
+		person2.setLastNames("Liisalainen");
+
+		List<Person> persons = new ArrayList<>();
+		persons.add(person);
+		persons.add(person2);
+
+		// System.out.println("tässä henkilöt" + objectMapper.writeValueAsString(persons));
+
+		when(personService.getPersonsByLastName(person2.getLastNames())).thenReturn(persons);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/persons/lastname/{lastName}", "Liisalainen")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	// TESTING UPDATING EMAIL - VALIDATION INCLUDED IN CONTROLLER - SERVICE CLASS
+	// TESTING AND CONTROLLER TESTING /persons/{id}/email
 	@Test
 	public void testUpdateEmail() throws Exception {
 		@SuppressWarnings("deprecation")
@@ -190,63 +258,23 @@ public class PersonControllerTest {
 
 		assertEquals(newEmail, person.getAddress().getEmail());
 	}
-	
-	// TESTING GET BY FIRSTNAME - TESTING SERVICE AND CONTROLLER /persons/firstname/{firstNames}
-	@Test
-	public void testGetByFirstName() throws Exception {
-		Person person = new Person();
-		Person person2 = new Person();
-		person.setFirstnames("Matti");
-		person.setLastNames("Mattilainen");
-		person2.setFirstnames("Liisa");
-		person2.setLastNames("Liisalainen");
-		
-		List<Person> persons = new ArrayList<>();
-		persons.add(person);
-		persons.add(person2);
-		
-		when(personService.getPersonsByFirstName(person.getFirstnames())).thenReturn(persons);
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/persons/firstname/{firstNames}", "Matti")
-				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk());
-		
-	}
-	
-	// TESTING GET BY LASTNAME - TESTING SERVICE AND CONTROLLER /persons/lastname/{lastName}
-	@Test
-	public void testGetByLastName() throws Exception {
-		Person person = new Person();
-		Person person2 = new Person();
-		person.setFirstnames("Matti");
-		person.setLastNames("Mattilainen");
-		person2.setFirstnames("Liisa");
-		person2.setLastNames("Liisalainen");
-		
-		List<Person> persons = new ArrayList<>();
-		persons.add(person);
-		persons.add(person2);
-		
-		// System.out.println("tässä henkilöt" + objectMapper.writeValueAsString(persons));
-		
-		when(personService.getPersonsByLastName(person2.getLastNames())).thenReturn(persons);
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/persons/lastname/{lastName}", "Liisalainen") // if trying to find Mattilainen then correct above person2.getLastNames > person.getLastNames
-				.contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk());
-	}
-	
+
+	// TEST DELETE EMAIL - TESTS CONTROLLER AND ADDRESSSERVICE
 	@Test
 	public void testDeleteEmail() throws Exception {
-	    Person person = new Person();
-	    Date date = new Date(120, 0, 11);
-	    Address address = new Address("Tuureporinkatu 15 b", "20100 Turku", date, 2, "Turku", date, "Turku",
-	            "juuso.testi@esimerkki.com", "+358 4001234");
-	    person.setId(11L);
-	    
-	    when(addressService.deleteEmail(eq(person.getId()))).thenReturn(null); // should return null
-	    
-	    mockMvc.perform(MockMvcRequestBuilders.delete("/persons/{id}/email", 11L)
-	            .contentType(MediaType.APPLICATION_JSON))
-	            .andExpect(MockMvcResultMatchers.status().isNotFound()); // waiting for 404 because no email after delete
+		Person person = new Person();
+		Date date = new Date(120, 0, 11);
+		Address address = new Address("Tuureporinkatu 15 b", "20100 Turku", date, 2, "Turku", date, "Turku",
+				"juuso.testi@esimerkki.com", "+358 4001234");
+		person.setId(11L);
+
+		when(addressService.deleteEmail(eq(person.getId()))).thenReturn(null); // should return null if deleted
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.delete("/persons/{id}/email", 11L)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isNotFound()); // waiting for 404 because no email after
+																			// delete
 	}
 
 }
