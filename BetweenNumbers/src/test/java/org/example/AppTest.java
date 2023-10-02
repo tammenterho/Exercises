96525455c0319f9eeb0aafccfab8824e62ca5662
tammenterho <terho.mikko@gmail.com>
2023-10-02 11:14:12 +0300

    test

--- BetweenNumbers/src/test/java/org/example/AppTest.java
+++ BetweenNumbers/src/test/java/org/example/AppTest.java
@@ -1,20 +1,29 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSmallestGap() {
+        ArrayList<Integer>numbers1 = new ArrayList<>();
+        numbers1.add(1);
+        numbers1.add(4);
+        numbers1.add(12);
+        numbers1.add(22);
+
+        ArrayList<Integer>numbers2 = new ArrayList<>();
+        numbers2.add(5);
+        numbers2.add(7);
+        numbers2.add(7);
+        numbers2.add(10);
+
+        assertEquals(2, App.smallestGap(numbers1));
+        assertEquals(1, App.smallestGap(numbers2));
+
     }
 }
