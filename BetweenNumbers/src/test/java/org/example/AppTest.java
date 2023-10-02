2989fd4a26baed434cc3c56c36d61b913d23ca93
tammenterho <terho.mikko@gmail.com>
2023-10-02 16:38:25 +0300

    test3

--- BetweenNumbers/src/test/java/org/example/AppTest.java
+++ BetweenNumbers/src/test/java/org/example/AppTest.java
@@ -16,14 +16,23 @@ public class AppTest
         numbers1.add(12);
         numbers1.add(22);
 
+        // double num test
         ArrayList<Integer>numbers2 = new ArrayList<>();
         numbers2.add(5);
         numbers2.add(7);
         numbers2.add(7);
         numbers2.add(10);
 
+        // same result test
+        ArrayList<Integer>numbers3 = new ArrayList<>();
+        numbers3.add(1);
+        numbers3.add(3);
+        numbers3.add(5);
+        numbers3.add(7);
+
         assertEquals(2, App.smallestGap(numbers1));
         assertEquals(1, App.smallestGap(numbers2));
+        assertEquals(1, App.smallestGap(numbers3));
 
     }
 }
